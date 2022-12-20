5217ebf28a0b8dbf4521a9e1de69558dfa6dd714
tgrall <tugdual@gmail.com>
2022-12-20 08:41:04 +0100

    initial commit

---
+++ src/main/java/com/tugdualgrall/demo/SimpleApp.java
@@ -0,0 +1,19 @@
+package com.tugdualgrall.demo;
+
+public class SimpleApp {
+
+    private String aStupidString = "Hello World";
+
+    public static void main(String[] args) {
+        System.out.println( "Address "+ a() );
+
+    }
+
+    private static String a() {
+        String address = "127.0.0.1";
+        return address;
+    }
+
+
+
+}
---
+++ src/test/java/com/tugdualgrall/demo/SimpleAppTest.java
@@ -0,0 +1,16 @@
+package com.tugdualgrall.demo;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class SimpleAppTest
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
